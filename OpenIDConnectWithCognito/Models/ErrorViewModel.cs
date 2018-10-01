a9de22eaa6908a019672e5f60070f95d54eb7ad0
Mike Bentzen <mike.bentzen@gmail.com>
2018-10-02 08:56:33 +1000

    Add project files.

---
+++ OpenIDConnectWithCognito/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenIDConnectWithCognito.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
